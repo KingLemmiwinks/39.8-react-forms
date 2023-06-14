336c4d49b29a1de2d41f41639453b2af3908a9c4
Anthony Ramos <ramosanthony@gmail.com>
2023-06-14 10:16:30 -0400

    Color Box App and Tests

---
+++ color-box-maker/src/Box.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Box.css";
+
+function Box({ idx, removeBox, width, height, boxBackgroundColor }) {
+  return (
+    <div>
+      <div
+        className="Box"
+        key={idx}
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+          backgroundColor: `${boxBackgroundColor}`,
+        }}
+      >
+        <button type="button" onClick={() => removeBox(idx)}>
+          Remove X
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Box;
